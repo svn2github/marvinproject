829e989de54ebb384dad9e2ff36c6d203bdb8b88
leirbag_arc <leirbag_arc@ccb5e5b0-b481-49e6-8c11-d5b444fbd5f0>
2014-06-05 00:39:40 +0000

    new plug-ins added to MarvinPluginCollection
    git-svn-id: http://svn.code.sf.net/p/marvinproject/code/dev@123 ccb5e5b0-b481-49e6-8c11-d5b444fbd5f0

--- trunk/MarvinFramework/src/marvin/MarvinPluginCollection.java
+++ trunk/MarvinFramework/src/marvin/MarvinPluginCollection.java
@@ -6,27 +6,59 @@ import java.util.List;
 import marvin.image.MarvinImage;
 import marvin.image.MarvinImageMask;
 import marvin.plugin.MarvinImagePlugin;
+import marvin.util.MarvinAttributes;
 import marvin.util.MarvinPluginLoader;
 
 public class MarvinPluginCollection {
 
 	private static MarvinImagePlugin	blackAndWhite,
 										brightnessAndContrast,
+										boundaryFill,
 										colorChannel,
 										combineByMask,
 										combineByTransparency,
 										convolution,
+										crop,
 										determineSceneBackground,
 										emboss,
+										flip,
 										gaussianBlur,
 										grayScale,
+										grayScaleQuantization,
+										histogramEqualization,
+										halftoneCircles,
+										halftoneDithering,
+										halftoneErrorDiffusion,
+										halftoneRylanders,
+										imageSlicer,
 										invertColors,
+										iteratedFuncionSystems,
+										juliaSet,
+										lindenmayer,
+										mandelbrot,
+										statisticalMaximum,
+										statisticalMedian,
+										statisticalMinimum,
+										statisticalMode,
 										mergePhotos,
 										moravec,
+										morphologicalBoundary,
+										morphologicalClosing,
+										morphologicalDilation,
+										morphologicalErosion,
+										morphologicalOpening,
 										pixelize,
+										prewitt,
+										roberts,
+										rotate,
+										scale,
+										sobel,
+										skew,
 										sepia,
+										subtract,
 										skinColorDetection,
 										television,
+										tileTexture,
 										thresholding;
 	
 	/*==============================================================================================
@@ -69,18 +101,18 @@ public class MarvinPluginCollection {
 	  ==============================================================================================*/
 	public static void combineByMask(MarvinImage imageA, MarvinImage imageB, MarvinImage imageOut, int x, int y, Color colorMask){
 		combineByMask = checkAndLoadImagePlugin(combineByMask, "org.marvinproject.image.combine.combineByMask");
-		colorChannel.setAttribute("xi", x);
-		colorChannel.setAttribute("yi", y);
-		colorChannel.setAttribute("combinationImage", imageB);
-		colorChannel.setAttribute("colorMask", colorMask);
-		colorChannel.process(imageA, imageOut);
+		combineByMask.setAttribute("xi", x);
+		combineByMask.setAttribute("yi", y);
+		combineByMask.setAttribute("combinationImage", imageB);
+		combineByMask.setAttribute("colorMask", colorMask);
+		combineByMask.process(imageA, imageOut);
 	}
 	
 	/*==============================================================================================
 	  | COMBINE BY TRANSPARENCY
 	  ==============================================================================================*/
 	public static void combineByTransparency(MarvinImage imageA, MarvinImage imageB, MarvinImage imageOut, int x, int y, int transparency){
-		combineByTransparency = checkAndLoadImagePlugin(combineByMask, "org.marvinproject.image.combine.combineByTransparency");
+		combineByTransparency = checkAndLoadImagePlugin(combineByTransparency, "org.marvinproject.image.combine.combineByTransparency");
 		combineByTransparency.setAttribute("xi", x);
 		combineByTransparency.setAttribute("yi", y);
 		combineByTransparency.setAttribute("combinationImage", imageB);
@@ -167,6 +199,45 @@ public class MarvinPluginCollection {
 	}
 	
 	/*==============================================================================================
+	  | HALFTONE CIRCLES
+	  ==============================================================================================*/
+	public static void halftoneCircles(MarvinImage imageIn, MarvinImage imageOut, int circleWidth, int shift, int circlesDistance, MarvinImageMask mask){
+		halftoneCircles = checkAndLoadImagePlugin(halftoneCircles, "org.marvinproject.image.halftone.circles");
+		halftoneCircles.setAttribute("circleWidth", circleWidth);
+		halftoneCircles.setAttribute("shift", shift);
+		halftoneCircles.setAttribute("circlesDistance", circlesDistance);
+		halftoneCircles.process(imageIn, imageOut, mask);
+	}
+	
+	public static void halftoneCircles(MarvinImage imageIn, MarvinImage imageOut, int circleWidth, int shift, int circlesDistance){
+		halftoneCircles(imageIn, imageOut, circleWidth, shift, circlesDistance, MarvinImageMask.NULL_MASK);
+	}
+	
+	/*==============================================================================================
+	  | HALFTONE DITHERING
+	  ==============================================================================================*/
+	public static void halftoneDithering(MarvinImage imageIn, MarvinImage imageOut){
+		halftoneDithering = checkAndLoadImagePlugin(halftoneDithering, "org.marvinproject.image.halftone.dithering");
+		halftoneDithering.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | HALFTONE ERROR DIFFUSION
+	  ==============================================================================================*/
+	public static void halftoneErrorDiffusion(MarvinImage imageIn, MarvinImage imageOut){
+		halftoneErrorDiffusion = checkAndLoadImagePlugin(halftoneErrorDiffusion, "org.marvinproject.image.halftone.errorDiffusion");
+		halftoneErrorDiffusion.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | HALFTONE RAYLANDERS
+	  ==============================================================================================*/
+	public static void halftoneRaylanders(MarvinImage imageIn, MarvinImage imageOut){
+		halftoneRylanders = checkAndLoadImagePlugin(halftoneRylanders, "org.marvinproject.image.halftone.rylanders");
+		halftoneRylanders.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
 	  | INVERT COLORS
 	  ==============================================================================================*/
 	public static void invertColors(MarvinImage image){
@@ -194,11 +265,58 @@ public class MarvinPluginCollection {
 	/*==============================================================================================
 	  | MORAVEC
 	  ==============================================================================================*/
-	public static void moravec(MarvinImage imageIn, MarvinImage imageOut, int matrixSize, int threshold){
+	public static MarvinAttributes moravec(MarvinImage imageIn, MarvinImage imageOut, int matrixSize, int threshold){
 		moravec = checkAndLoadImagePlugin(moravec, "org.marvinproject.image.corner.moravec");
 		moravec.setAttribute("matrixSize", matrixSize);
 		moravec.setAttribute("threshold", threshold);
-		moravec.process(imageIn, imageOut);
+		MarvinAttributes ret = new MarvinAttributes();
+		moravec.process(imageIn, imageOut, ret);
+		return ret;
+	}
+	
+	/*==============================================================================================
+	  | MORPHOLOCIAL BOUNDARY
+	  ==============================================================================================*/
+	public static void morphologicalBoundary(MarvinImage imageIn, MarvinImage imageOut, boolean[][] matrix){
+		morphologicalBoundary = checkAndLoadImagePlugin(morphologicalBoundary, "org.marvinproject.image.morphological.boundary");
+		morphologicalBoundary.setAttribute("matrix", matrix);
+		morphologicalBoundary.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | MORPHOLOCIAL CLOSING
+	  ==============================================================================================*/
+	public static void morphologicalClosing(MarvinImage imageIn, MarvinImage imageOut, boolean[][] matrix){
+		morphologicalClosing = checkAndLoadImagePlugin(morphologicalClosing, "org.marvinproject.image.morphological.closing");
+		morphologicalClosing.setAttribute("matrix", matrix);
+		morphologicalClosing.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | MORPHOLOCIAL DILATION
+	  ==============================================================================================*/
+	public static void morphologicalDilation(MarvinImage imageIn, MarvinImage imageOut, boolean[][] matrix){
+		morphologicalDilation = checkAndLoadImagePlugin(morphologicalDilation, "org.marvinproject.image.morphological.dilation");
+		morphologicalDilation.setAttribute("matrix", matrix);
+		morphologicalDilation.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | MORPHOLOCIAL EROSION
+	  ==============================================================================================*/
+	public static void morphologicalErosion(MarvinImage imageIn, MarvinImage imageOut, boolean[][] matrix){
+		morphologicalErosion = checkAndLoadImagePlugin(morphologicalErosion, "org.marvinproject.image.morphological.erosion");
+		morphologicalErosion.setAttribute("matrix", matrix);
+		morphologicalErosion.process(imageIn, imageOut);
+	}
+	
+	/*==============================================================================================
+	  | MORPHOLOCIAL OPENING
+	  ==============================================================================================*/
+	public static void morphologicalOpening(MarvinImage imageIn, MarvinImage imageOut, boolean[][] matrix){
+		morphologicalOpening = checkAndLoadImagePlugin(morphologicalOpening, "org.marvinproject.image.morphological.opening");
+		morphologicalOpening.setAttribute("matrix", matrix);
+		morphologicalOpening.process(imageIn, imageOut);
 	}
 	
 	/*==============================================================================================
@@ -212,6 +330,30 @@ public class MarvinPluginCollection {
 		pixelize = checkAndLoadImagePlugin(pixelize, "org.marvinproject.image.blur.pixelize");
 		pixelize.setAttribute("squareSide", squareSide);
 		pixelize.process(imageIn, imageOut, mask);
+	}
+	
+	/*==============================================================================================
+	  | PREWITT
+	  ==============================================================================================*/
+	public static void prewitt(MarvinImage imageIn, MarvinImage imageOut){
+		prewitt.process(imageIn, imageOut, MarvinImageMask.NULL_MASK);
+	}
+	
+	public static void prewitt(MarvinImage imageIn, MarvinImage imageOut, MarvinImageMask mask){
+		prewitt = checkAndLoadImagePlugin(prewitt, "org.marvinproject.image.edge.prewitt");
+		prewitt.process(imageIn, imageOut, mask);
+	}
+	
+	/*==============================================================================================
+	  | ROBERTS
+	  ==============================================================================================*/
+	public static void roberts(MarvinImage imageIn, MarvinImage imageOut){
+		roberts.process(imageIn, imageOut, MarvinImageMask.NULL_MASK);
+	}
+	
+	public static void roberts(MarvinImage imageIn, MarvinImage imageOut, MarvinImageMask mask){
+		roberts = checkAndLoadImagePlugin(roberts, "org.marvinproject.image.edge.roberts");
+		roberts.process(imageIn, imageOut, mask);
 	}
 	
 	/*==============================================================================================
@@ -232,6 +374,18 @@ public class MarvinPluginCollection {
 	}
 	
 	/*==============================================================================================
+	  | SOBEL
+	  ==============================================================================================*/
+	public static void sobel(MarvinImage imageIn, MarvinImage imageOut){
+		sobel.process(imageIn, imageOut, MarvinImageMask.NULL_MASK);
+	}
+	
+	public static void sobel(MarvinImage imageIn, MarvinImage imageOut, MarvinImageMask mask){
+		sobel = checkAndLoadImagePlugin(sobel, "org.marvinproject.image.edge.sobel");
+		sobel.process(imageIn, imageOut, mask);
+	}
+	
+	/*==============================================================================================
 	  | SKIN COLOR DETECTION
 	  ==============================================================================================*/
 	public static void skinColorDetection(MarvinImage image){
@@ -246,6 +400,7 @@ public class MarvinPluginCollection {
 		skinColorDetection = checkAndLoadImagePlugin(skinColorDetection, "org.marvinproject.image.color.skinColorDetection");
 		skinColorDetection.process(imageIn, imageOut, mask);
 	}
+	
 	
 	/*==============================================================================================
 	  | TELEVISION
@@ -276,6 +431,7 @@ public class MarvinPluginCollection {
 	
 	public static void thresholding(MarvinImage imageIn, MarvinImage imageOut, int threshold, MarvinImageMask mask){
 		thresholding = checkAndLoadImagePlugin(thresholding, "org.marvinproject.image.color.thresholding");
+		thresholding.setAttribute("threshold",threshold);
 		thresholding.process(imageIn, imageOut, mask);
 	}
 	
